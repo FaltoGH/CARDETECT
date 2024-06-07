0415a669c9bff6309395047a79274ab0e57ec4d5
SoobumNa <sb051380us@gmail.com>
2024-06-07 21:13:27 +0900

    Train yolov8n.yaml

---
+++ __main__.py
@@ -0,0 +1,13 @@
+import os
+dirname = os.path.dirname(__file__)
+os.chdir(dirname)
+datayaml=os.path.join(dirname, "Playing Cards.v3-original_raw-images.yolov8", "data.yaml")
+if not os.path.isfile(datayaml):
+    raise FileNotFoundError(datayaml)
+import datetime
+from ultralytics import YOLO
+yolo=YOLO("yolov8n.yaml")
+if __name__ == "__main__":
+    print(datetime.datetime.now())
+    yolo.train(task="detect",mode="train",data=datayaml,device=0,patience=2,verbose=True,epochs=100)
+    print(datetime.datetime.now())
